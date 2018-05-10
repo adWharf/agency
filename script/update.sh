fc383d630394120e518eec9ea57d60913ad1fbb6
IamBusy <1342247033@qq.com>
2018-05-10 11:41:10 +0800

    Fix bug

--- script/update.sh
+++ script/update.sh
@@ -1,4 +1,4 @@
 docker stop agency || true
 docker rm -v agency || true
 docker build -t agency .
-docker run -v 5000:5000 --rm --name agency agency
+docker run -p 5000:5000 --rm --name agency agency
