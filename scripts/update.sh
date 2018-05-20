dc97112011638b730fb1e47ffb814ad8f701b17c
IamBusy <1342247033@qq.com>
2018-05-20 10:37:25 +0800

    :bookmark_tabs:: fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,4 @@
 docker stop agency || true
 docker rm -v agency || true
 docker build -t agency .
-docker run -p 5000:5000 --rm --name agency --restart=always -d agency
+docker run -p 5000:5000 --name agency --restart=always -d agency
