2b4e74a742f96ac386bcb0050adf392652115af4
IamBusy <1342247033@qq.com>
2018-05-20 10:33:58 +0800

    :bookmark_tabs:: retart container auto

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 docker stop agency || true
 docker rm -v agency || true
 docker build -t agency .
-docker run -p 5000:5000 --rm --name agency agency
+docker run -p 5000:5000 --rm --name agency --restart=always -dagency
